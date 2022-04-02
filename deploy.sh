e8ec47a48edcff73ed5f87fd3da77d6cbb2e57c3
super-cow-powers <dm1306@york.ac.uk>
2022-04-02 23:08:45 +0100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+
+repo_uri="https://x-access-token:${ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+remote_name="origin"
+
 pip install -r requirements.txt
 
 python3 shock.py build
@@ -7,7 +11,8 @@ python3 shock.py build
 cd build/
 # create a fresh new git repo in the output directory
 git init
+git switch master
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:ShockSoc/ShockSoc.github.io.git publish:gh-deploy
+git push -f "$repo_uri" master:gh-deploy
